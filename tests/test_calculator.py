8c6f9017c84ab57fa726586041009ad39c6b86ba
TanmayRaipure <you@example.com>
2026-01-05 23:42:05 +0530

    first commit

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,11 +1,11 @@
 import pytest
-from src.calculator import add, substract, multiply, divide
+from src.calculator import add, subtract, multiply, divide
 
 def test_add():
     assert add(2, 3) == 5
 
-def test_substract():
-    assert substract(5, 3) == 2
+def test_subtract():
+    assert subtract(5, 3) == 2
 
 def test_multiply():
     assert multiply(4, 3) == 12
@@ -16,4 +16,4 @@ def test_divide():
 def test_divide_by_zero():
     with pytest.raises(ValueError):
         divide(10 ,0)
-        
+
