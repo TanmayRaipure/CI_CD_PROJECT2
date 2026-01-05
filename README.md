8adbe08bd518fdfd98d8f702c5c820a522bf4f56
TanmayRaipure <you@example.com>
2026-01-05 17:33:49 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## This is a CICD projrct
+## with github actions
---
+++ tests/test_calculator.py
@@ -0,0 +1,19 @@
+import pytest
+from src.calculator import add, substract, multiply, divide
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_substract():
+    assert substract(5, 3) == 2
+
+def test_multiply():
+    assert multiply(4, 3) == 12
+
+def test_divide():
+    assert divide(10, 2) == 5
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        divide(10 ,0)
+        
